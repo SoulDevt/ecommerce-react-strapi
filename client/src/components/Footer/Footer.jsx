bf7a6db0ea2eb866cb81c124961cff87f7f7a7b2
SoulDevt <souleyboyz@gmail.com>
2022-12-13 13:00:48 +0100

    design navbar and footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+import './Footer.scss';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <div className="top">
+        <div className="item">
+          <h1>Categories</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Shoes</span>
+          <span>Accessories</span>
+          <span>New Arrivals</span>
+        </div>
+        <div className="item">
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+        <div className="item">
+          <h1>About</h1>
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+            culpa ex, molestias est veniam dicta qui aperiam. Fuga ullam omnis
+            asperiores tempore quod doloribus, commodi cupiditate, blanditiis
+            architecto ad ipsum!
+          </span>
+        </div>
+        <div className="item">
+          <h1>Contact</h1>
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea beatae
+            iusto vitae in nihil error quo veritatis! Impedit itaque, cum porro
+            numquam laborum excepturi culpa sequi id illum tempora veritatis.
+          </span>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <span className="logo">SouleyShop</span>
+          <span className="copyright">© Copyright 2023. All Rights Reserved</span>
+        </div>
+        <div className="right">
+          <img src="/img/payment.png" alt="all payments methods" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
