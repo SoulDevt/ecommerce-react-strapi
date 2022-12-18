2f55a21cc64da61916c7ba7e30a68171f94d0346
SoulDevt <souleyboyz@gmail.com>
2022-12-19 00:01:16 +0100

    design products and products card

---
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Card from "../Card/Card";
+import "./FeaturedProducts.scss";
+
+const FeaturedProducts = ({ type }) => {
+  const data = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs-tinysrab&w=1600",
+      title: "halt",
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs-tinysrab&w=1600",
+      title: "halt",
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs-tinysrab&w=1600",
+      title: "halt",
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 4,
+      img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs-tinysrab&w=1600",
+      title: "halt",
+      oldPrice: 19,
+      price: 12,
+    },
+  ];
+  return (
+    <div className="featuredProducts">
+      <div className="top">
+        <h1>{type} products</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis,
+          consequatur fugiat inventore iste at corrupti commodi veritatis veniam
+          animi itaque est nesciunt tempora! Maiores et ex laborum aut cum
+          beatae!
+        </p>
+      </div>
+      <div className="bottom">
+        {data.map(item => (
+          <Card item={item} key={item.id}/>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProducts;
