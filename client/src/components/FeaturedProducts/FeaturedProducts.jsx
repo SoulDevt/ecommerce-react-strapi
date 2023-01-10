83216c2d52ac543c1c073e7f7af7f93bb4d89674
SoulDevt <souleyboyz@gmail.com>
2023-01-10 18:16:31 +0100

    connection react strapi & fetch featured products

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,40 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../Card/Card";
 import "./FeaturedProducts.scss";
+import axios from "axios"
 
 const FeaturedProducts = ({ type }) => {
-  const data = [
-    {
-      id: 1,
-      img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs-tinysrab&w=1600",
-      img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs-tinysrab&w=1600%22",
-      isNew: true,
-      title: "halt",
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 2,
-      img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs-tinysrab&w=1600",
-      title: "halt",
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 3,
-      img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs-tinysrab&w=1600",
-      title: "halt",
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 4,
-      img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs-tinysrab&w=1600",
-      title: "halt",
-      oldPrice: 19,
-      price: 12,
-    },
-  ];
+  // const data = [
+  //   {
+  //     id: 1,
+  //     img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs-tinysrab&w=1600",
+  //     img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs-tinysrab&w=1600%22",
+  //     isNew: true,
+  //     title: "halt",
+  //     oldPrice: 19,
+  //     price: 12,
+  //   },
+  //   {
+  //     id: 2,
+  //     img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs-tinysrab&w=1600",
+  //     title: "halt",
+  //     oldPrice: 19,
+  //     price: 12,
+  //   },
+  //   {
+  //     id: 3,
+  //     img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs-tinysrab&w=1600",
+  //     title: "halt",
+  //     oldPrice: 19,
+  //     price: 12,
+  //   },
+  //   {
+  //     id: 4,
+  //     img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs-tinysrab&w=1600",
+  //     title: "halt",
+  //     oldPrice: 19,
+  //     price: 12,
+  //   },
+  // ];
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(process.env.REACT_APP_API_URL + "/products?populate=*", {
+          headers : {
+            Authorization: "Bearer " + process.env.REACT_APP_API_TOKEN
+          }
+        })
+        setData(res.data.data)
+      } catch(err) {
+        console.log(err)
+      }
+    }
+    fetchData()
+  },[])
+
+
   return (
     <div className="featuredProducts">
       <div className="top">
