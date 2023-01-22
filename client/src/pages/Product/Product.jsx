3b791a2afca134cac5f32bb3286c46b2d62a74c8
SoulDevt <souleymane.gueye.pro@gmail.com>
2023-01-23 00:14:59 +0100

    fetch single product and sort products

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -3,29 +3,41 @@ import "./Product.scss";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import BalanceIcon from "@mui/icons-material/Balance";
+import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
 
 const Product = () => {
-  const [selectedImg, setSelectedImg] = useState(0);
+  
+  const id = useParams().id;
+  const [selectedImg, setSelectedImg] = useState("img");
   const [quantity, setQuantity] = useState(1);
 
-  const images = [
-    "https://images.pexels.com/photos/10026491/pexels-photo-10026491.png?auto=compress&cs-tinysrab&w=1600",
-    "https://images.pexels.com/photos/12179283/pexels-photo-12179283.jpeg?auto=compress&cs-tinysrab&w=1600",
-  ];
+  const { data, loading, error } = useFetch(
+    `/products/${id}?populate=*`
+  );
+
+  console.log(data)
+
+  // const images = [
+  //   "https://images.pexels.com/photos/10026491/pexels-photo-10026491.png?auto=compress&cs-tinysrab&w=1600",
+  //   "https://images.pexels.com/photos/12179283/pexels-photo-12179283.jpeg?auto=compress&cs-tinysrab&w=1600",
+  // ];
   return (
     <div className="product">
+      {loading ? "loading" :
+      <>
       <div className="left">
         <div className="images">
-          <img src={images[0]} alt="" onClick={(e) => setSelectedImg(0)} />
-          <img src={images[1]} alt="" onClick={(e) => setSelectedImg(1)} />
+          <img src={process.env.REACT_APP_UPLOAD_URL + data?.attributes?.img?.data?.attributes?.url} alt="" onClick={(e) => setSelectedImg("img")} />
+          <img src={process.env.REACT_APP_UPLOAD_URL + data?.attributes?.img2?.data?.attributes?.url} alt="" onClick={(e) => setSelectedImg("img2")} />
         </div>
         <div className="mainImg">
-          <img src={images[selectedImg]} alt="" />
+          <img src={process.env.REACT_APP_UPLOAD_URL + data?.attributes[selectedImg]?.data?.attributes?.url} alt="" />
         </div>
       </div>
       <div className="right">
-        <h1>Title</h1>
-        <span className="price">199€</span>
+        <h1>{data?.attributes?.title}</h1>
+        <span className="price">{data?.attributes?.price}€</span>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
           molestiae delectus nihil nisi possimus, explicabo quibusdam at
@@ -67,6 +79,8 @@ const Product = () => {
           <span>FAQ</span>
         </div>
       </div>
+      </>
+      }
     </div>
   );
 };
